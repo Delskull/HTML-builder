65e22ce29e0f0e0c8a2266703dc3fc98573bf41c
Sergey Stepanov <Deluta1989@gmail.com>
2024-10-23 12:03:50 +0300

    feat: complete task 05-merge-styles

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const originalPath = path.join(__dirname, 'files')
-const copyPath = path.join(__dirname, 'files-copy')
+const originalPath = path.join(__dirname, 'files');
+const copyPath = path.join(__dirname, 'files-copy');
 
 const removeDir = async () => {
   try {
@@ -13,33 +13,29 @@ const removeDir = async () => {
 };
 
 const createCopyFolder = async () => {
-  return new Promise((resolve,reject) => {
-    fs.mkdir(copyPath,{recursive:true},(err) => {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(copyPath, { recursive: true }, (err) => {
       if (err) {
-        reject(err)
+        reject(err);
+      } else {
+        resolve();
       }
-      else {
-        resolve()
-      }
-    })
-  })
-}
+    });
+  });
+};
 const copyFile = async () => {
   try {
-   await removeDir()
-   await createCopyFolder()
-    const files = await fs.promises.readdir(originalPath)
-    for (const file of files){
-      const originPath = path.join(originalPath,file)
-      const copyFolder = path.join(copyPath,file)
-      await fs.promises.copyFile(originPath,copyFolder)
-
+    await removeDir();
+    await createCopyFolder();
+    const files = await fs.promises.readdir(originalPath);
+    for (const file of files) {
+      const originPath = path.join(originalPath, file);
+      const copyFolder = path.join(copyPath, file);
+      await fs.promises.copyFile(originPath, copyFolder);
     }
-    console.log('дело сделано')
+    console.log('дело сделано');
+  } catch (err) {
+    console.log('ошибка', err);
   }
-  catch (err) {
-    console.log('ошибка',err)
-  }
-}
-copyFile()
-
+};
+copyFile();
---
+++ 05-merge-styles/index.js
@@ -0,0 +1,63 @@
+const fs = require('fs');
+const path = require('path');
+const { rejects } = require('node:assert');
+
+const mergePath = path.join(__dirname, 'project-dist', 'bundle.css');
+const stylesPath = path.join(__dirname, 'styles');
+
+const writeFile = async (path, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, data, (err) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve();
+    });
+  });
+};
+
+const checkDirectoryAsync = async (path, data) => {
+  return new Promise((resolve, reject) =>
+    fs.readdir(path, (err, data) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve(data);
+    }),
+  );
+};
+
+const readFileAsync = async (path) => {
+  return new Promise((resolve, reject) =>
+    fs.readFile(path, { encoding: 'utf-8' }, (err, data) => {
+      if (err) {
+        return reject(err.message);
+      }
+      resolve(data);
+    }),
+  );
+};
+
+writeFile(mergePath, 'bundle.css')
+  .then(() => {
+    console.log('Файл успешно создан');
+    return checkDirectoryAsync(stylesPath);
+  })
+  .then((files) => {
+    const cssFiles = files.filter((file) => {
+      const extname = path.extname(file);
+      return extname === '.css';
+    });
+    const readPromises = cssFiles.map((file) => {
+      const filePath = path.join(stylesPath, file);
+      return readFileAsync(filePath);
+    });
+    console.log('все промисы прочитаны и собраны');
+    return Promise.all(readPromises);
+  })
+  .then((text) => {
+    const mergeAllText = text.join('\n');
+    return writeFile(mergePath, mergeAllText);
+  })
+
+  .catch((err) => console.log(err));
